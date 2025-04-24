fbd0adf0d93583d63866d0b232dcb5d088253eac
Sujoy Paul <freelancersujoy@gmail.com>
2025-04-24 10:45:27 +0600

    webhook set problem solved

--- setWebhook.js.js
+++ setWebhook.js.js
@@ -2,7 +2,7 @@ let axios = require("axios")
 require("dotenv").config()
 
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const WEBHOOK_URL = 'https://follow-builder-bot.vercel.app/';
+const WEBHOOK_URL = 'https://follow-builder-bot.vercel.app/api/bot';
 
 axios.get(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=${WEBHOOK_URL}`)
   .then(res => console.log(res.data))
